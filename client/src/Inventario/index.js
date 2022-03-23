9320dde7d85e30394fe4713d71aa64a1cb96c06a
Axel Beltrán <89809580+axelll493@users.noreply.github.com>
2022-03-22 22:55:22 -0300

    Add files via upload

---
+++ client/src/Inventario/index.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id:1,
+        name:"Tenis deportivos",
+        image: "https://m.media-amazon.com/images/I/81xE94ijiNL._AC_UX500_.jpg",
+        detalle: {
+            Marca:""
+        }
+    },
+    {
+        id:2,
+        name: "Zapatoss de baile",
+        image: "https://m.media-amazon.com/images/I/81pJWs1j2pL._AC_UY500_.jpg",
+        producType: "salsa, tango, vals, latino, swing dance",
+        price: 99.99,
+        rating: 4,
+        description: "El zapato de baile ancho Adrian es muy fino para hombre. Con un ancho extra en la bola del pie y la caja de los dedos, los pies anchos finalmente pueden sentirse cómodos bailando, mientras que todavía se ven elegantes. El Adrian viene con soporte de arco superior y amortiguación interna para una sensación de comodidad premium, y está construido pensando en la transpirabilidad y la prevención de ampollas. Al igual que todos los zapatos de baile muy finos, el Adrian está hecho a mano profesionalmente completamente y utiliza ojales sin níquel."
+    },
+    {
+        id: 3,
+        name: "Deportivas",
+        image: "https://m.media-amazon.com/images/I/71dzaYg56AS._AC_UL320_.jpg",
+        producType: "Under Armour Charged Assert 9",
+        price: 60.09,
+        rating: 5,
+        description: "La entresuela Charged Cushioning utiliza espuma moldeada por compresión para una máxima capacidad de respuesta y durabilidad. Suela de goma sólida que cubre zonas de alto impacto para una mayor durabilidad con menos peso"
+    },
+    {
+        id: 4,
+        name: "Deportivas",
+        image: "https://m.media-amazon.com/images/I/71J7UawwfxL._AC_UL320_.jpg",
+        producType: "Skechers Delson-Camden",
+        price: 60.13,
+        rating: 5,
+        description: "Sin cordones, con puntera redonda de malla con cordón elástico tipo encaje."
+    }
+]
+
