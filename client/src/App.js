0ba5580b9e1da488b6b456825c0d705ade6ad0b5
Axel Beltrán <89809580+axelll493@users.noreply.github.com>
2022-03-29 19:58:26 -0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import Products from './components/Products';
-import NavBar from './components/NavBar'
+
 import CheckoutPage from './components/CheckoutPage';
 import {Route, Routes } from 'react-router';
 import {BrowserRouter as Router} from 'react-router-dom'
+import SignIn from './components/SignIn';
+import Checkout from './components/Register/Register';
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <NavBar/>
- 
+
       
       <Routes>
           <Route exact path="/checkout-page" element={<CheckoutPage/>} />
-
+          <Route exact path="/SingIn" element={<SignIn/>}/>
+          <Route exact path="/Register" element={<Checkout/>}/>  
           <Route exact path="/" element={<Products/>} />
         </Routes>
     </div>
---
+++ client/src/components/Register/Register.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import AddressForm from './AddressForm';
+import PaymentForm from './PaymentForm';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">  
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  Direccion: {
+    marginBottom: theme.spacing(3)
+  }
+}));
+
+
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
+
+export default function Checkout() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+  const steps = ['1', '2'];
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+
+      </AppBar>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h8" align="center">
+            Logo Empresa
+          </Typography>
+          <Typography variant="h4" align="center" className={classes.Direccion}>
+            Agrega tu direccion
+          </Typography>
+          <React.Fragment>
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <div className={classes.buttons}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} className={classes.button}>
+                      Atras
+                    </Button>
+                  )}
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Aceptar y crear cuenta' : 'Siguiente'}
+                  </Button>
+                </div>
+              </React.Fragment>
+          </React.Fragment>
+        </Paper>
+        <Copyright />
+      </main>
+    </React.Fragment>
+  );
+}
