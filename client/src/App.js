850c051891dd1ed036267dc80c56c73b2d3cf6ce
Axel Beltrán <89809580+axelll493@users.noreply.github.com>
2022-04-01 14:18:10 -0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Products from './components/Products';
 import NavBarUser from './components/User/NavBarUser'
-import NavBarGuest from './components/Guest/NavBarGuest'
 import CheckoutPage from './components/CheckoutPage';
 import CheckoutCard from './components/CheckoutCard';
 import {Route, Routes } from 'react-router';
@@ -9,12 +8,14 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import SignIn from './components/SignIn';
 import RegistroForm from './components/Registro/Registro';
 import Categories from './components/Categories';
+import CrearProducto from './components/Agregar producto/CrearProducto';
 
 function App() {
   return (
     <Router>
     <div className="App">  
       <Routes>
+        <Route exact path="/CrearProduct" element={<CrearProducto/>}/>
           <Route exact path="/" element={<Products/>}/>
           <Route exact path="/user/profile" element={<NavBarUser/>}/>
           <Route exact path="/checkout-page" element={<CheckoutPage/>} />
