7de77725a17f7e7cfaeaa1c73fecb754689ab95b
Axel Beltrán <89809580+axelll493@users.noreply.github.com>
2022-04-04 20:17:50 -0300

    gg

---
+++ client/src/components/EditarYBorrarProductos/Editar.jsx
@@ -0,0 +1,221 @@
+import React, {useEffect, useState}  from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+    Table,
+    Button, 
+    Container,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    FormGroup,
+    ModalFooter,
+} from "reactstrap";
+import { getProducts, UpdateProduct, DeleteProduct } from "../../actions";
+
+
+
+
+
+const Editar = () => {
+    const dispatch = useDispatch()
+    const Products = useSelector((state) => state.products);
+    const[input, setstate]= useState({
+        name:"",
+        price: "",
+        stock: "",
+        description:"",
+        image:""
+    })
+    const [refresh]=useState("")
+    const {name, price, stock,description, image}= input
+const [modalInsertar, setStateModalInsectar] = useState(false)
+
+  
+    const mostrarModalInsertar= (e)=> {
+        setstate({
+            name: e.name,
+            price: e.price,
+            stock: e.stock,
+            description: e.description,
+            image:e.image
+        })
+        setStateModalInsectar({
+            modalInsertar:true,
+        });
+      };
+      const handleChange = (e) => {
+
+        setstate({
+          ...input,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [dispatch, refresh])
+    const handleSubmit = ()=>{
+        dispatch(UpdateProduct(input))
+    }
+    const handleDelete = (e, id) => {
+        e.preventDefault()
+        var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+id)
+        if(opcion===true){
+     
+          dispatch(DeleteProduct(id))
+        }
+    }
+
+
+    return (
+        <>
+            <Container>
+                <br />
+                <h1>Productos en Venta</h1>
+                <br />
+                <br />
+                <Table className="table">
+                    <thead>
+                        <tr>
+
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Stock</th>
+                            <th>Descripcion</th>
+                            <th>Imagen</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {Products.map((el)=>(
+                            <tr key={el.id}>
+
+                                <td>{el.name}</td>
+                                <td>{el.price}</td>
+                                <td>{el.stock}</td>
+                                <td style={{minWidth:"80vh"}}>{el.description}</td>
+                                <td>{el.image}</td>
+                                <td>
+                                    <Button
+                                        color="primary"
+                                        onClick={() => mostrarModalInsertar(el)}
+                                    >
+                                        Editar
+                                    </Button>
+                                    </td>
+                                    <td>
+                                    <Button 
+                                            type="submit"
+                                            color="danger" 
+                                            onClick={(e) => handleDelete(e, el.id)}
+                                            
+                                    >Eliminar</Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Container>
+
+            <Modal isOpen={modalInsertar} onRequestClose={()=>setStateModalInsectar(false)}>
+                <ModalHeader>
+                    <div><h3>Editar Productos</h3></div>
+                </ModalHeader>
+                <form onSubmit={handleSubmit}>
+                <ModalBody>
+                    <FormGroup>
+                        <label>
+                            Nombre Del Producto
+                        </label>
+
+                        <input
+                            className="form-control"
+                            name="name"
+                            type="text"
+                            value={name}
+                            onChange={handleChange}
+                        />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label>
+                        Precio Del Producto
+                        </label>
+                        <input
+                            className="form-control"
+                            name="price"
+                            type="number"
+                            value={price}
+                            onChange={handleChange}
+                        />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label>
+                            Stock:
+                        </label>
+                        <input
+                            className="form-control"
+                            name="stock"
+                            type="number"
+                            value={stock}
+                            onChange={handleChange}
+
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label>
+                            Descripcion:
+                        </label>
+                        <input
+                            className="form-control"
+                            name="description"
+                            type="text"
+                            value={description}
+                            onChange={handleChange}
+
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label>
+                            Imagen:
+                        </label>
+                        <input
+                            className="form-control"
+                            name="image"
+                            type="text"
+                            value={image}
+                            onChange={handleChange}
+                        />
+                    </FormGroup>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button
+                        color="primary"
+                        onClick={() => setStateModalInsectar(false)}
+                        type="submit"
+                    >
+                        Editar
+                    </Button>
+                    <Button
+                        color="danger"
+                        onClick={() => setStateModalInsectar(false)}
+                    >
+                        Cancelar
+                    </Button>
+                </ModalFooter>
+                 </form>
+            </Modal>
+           
+        </>
+    );
+
+}
+export default Editar;
+
