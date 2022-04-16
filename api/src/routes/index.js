5e3f57adc9dabaa86838d477582fbc25548da398
Damián Bastos <89169505+DamiBastos@users.noreply.github.com>
2022-04-16 18:04:12 -0300

    review

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,6 +8,8 @@ const categoriesRoute = require('./Categories');
 const usersRoute = require('./Users');
 const mercadoPagoRoute = require('./Mercadopago');
 const ordersRoute = require('./Orders')
+const wishlistRoute = require('./Wishlist');
+const reviewsRoute = require('./Review')
 
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -16,7 +18,8 @@ router.use('/', productsRoute);
 router.use('/', categoriesRoute);
 router.use('/', mercadoPagoRoute);
 router.use('/', ordersRoute)
-
+router.use('/', wishlistRoute);
+router.use('/', reviewsRoute)
 
 //////////////////////////////////////////////////////////////////////////////////
 module.exports = router;
